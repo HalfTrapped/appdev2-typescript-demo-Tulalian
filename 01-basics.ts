7b1fef16957678eb3c36eaf2634e6e815d3b9940
Aaron Matthew Tulalian <asianwater1@gmail.com>
2026-03-27 15:53:35 +0800

    Done playing and updating the 01-basic.ts file

--- 01-basics.ts
+++ 01-basics.ts
@@ -1,20 +1,20 @@
 // console.log("Hello World!")
 
-let userName: string = "Elmer";
-let userAge = 31;
+let userName: string = "Matthew";
+let userAge = 21;
 
 // ...
 
-userAge = "31"
+userAge = 21
 
 console.log(`Username - ${userName}`)
 console.log(`Age - ${userAge}`)
 
-function add(a: number, b = 5) {
+function add(a: number, b = 1) {
     return a + b
 }
 
-add(10)
+console.log(add(11))
 // add('10')
-add(10, 6)
-// add(10, '6')
+console.log(add(10, 20))
+// console.log(add(10, '6'))
